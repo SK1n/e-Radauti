8d3cda656cf592af84939cc2fbb10924b522851c
Luis <luys50734@gmail.com>
2020-10-04 23:56:59 +0300

    Added header to WebView

--- lib/jobs/announcements_web_view.dart
+++ lib/jobs/announcements_web_view.dart
@@ -42,18 +42,84 @@ class _AnnouncementWebViewState extends State<AnnouncementWebView> {
         ],
       ),
       drawer: NavDrawer2(),
-      body: IndexedStack(
-        index: _stackToView,
+      body: Column(
         children: [
-          WebView(
-            initialUrl: widget.slug,
-            onPageFinished: (String url) {
-              setState(() {
-                _stackToView = 0;
-              });
-            },
+          Container(
+            padding: EdgeInsets.only(
+              top: 20,
+              bottom: 15,
+            ),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_left,
+                      color: Color(0xFF979797),
+                    ), //_left Icons.arrow_back
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 80,
+                  child: new Stack(
+                    alignment: AlignmentDirectional.center,
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Icon(
+                            Icons.announcement,
+                            color: Color(0x55FB6340),
+                            size: 30,
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(
+                                35.0,
+                                6.0,
+                                0.0,
+                                0.0,
+                              ), //10.0 //25.0
+                              child: Text(
+                                'Anunțuri',
+                                style: TextStyle(
+                                  color: Color(0xFF000000), //Color(0xFFFFFFFF),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 19,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          Container(child: Center(child: CircularProgressIndicator())),
+          IndexedStack(
+            index: _stackToView,
+            children: [
+              SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height - 163,
+                  child: WebView(
+                    initialUrl: widget.slug,
+                    onPageFinished: (String url) {
+                      setState(() {
+                        _stackToView = 0;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Container(child: Center(child: CircularProgressIndicator())),
+            ],
+          ),
         ],
       ),
     );
