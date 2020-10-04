288bf1db0b009b5b036b570bfcb143ed5d03405c
Paul Coman <coman.paul@yahoo.com>
2020-10-04 18:12:33 -0400

    Actualizat master

--- lib/jobs/announcements_web_view.dart
+++ lib/jobs/announcements_web_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutterapperadauti/menu_page.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class AnnouncementWebView extends StatefulWidget {
   final String slug;
@@ -11,17 +13,31 @@ class AnnouncementWebView extends StatefulWidget {
 
 class _AnnouncementWebViewState extends State<AnnouncementWebView> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  num _stackToView = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        automaticallyImplyLeading: false,
+        title: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Icon(
+            Icons.announcement,
+            color: Color(0x55FB6340),
+            size: 30,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+          ),
+          Text('Anunturi'),
+        ]),
         leading: Container(
-          padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
-          margin: const EdgeInsets.fromLTRB(15.0, 5.0, 0.0, 5.0),
-          child: Image.asset("assets/logo_images/app_logo.png"),
+          child: FlatButton(
+            child: Icon(Ionicons.ios_arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
         actions: <Widget>[
           Container(
@@ -33,13 +49,33 @@ class _AnnouncementWebViewState extends State<AnnouncementWebView> {
                 Icons.menu,
                 size: 24,
                 color: Colors.black,
-              ), //Colors.white
+              ),
               onPressed: () => _scaffoldKey.currentState.openDrawer(),
             ),
           ),
         ],
       ),
       drawer: NavDrawer2(),
+      body: IndexedStack(
+        index: _stackToView,
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              height: MediaQuery.of(context).size.height,
+              child: WebView(
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: widget.slug,
+                onPageFinished: (String url) {
+                  setState(() {
+                    _stackToView = 0;
+                  });
+                },
+              ),
+            ),
+          ),
+          Container(child: Center(child: CircularProgressIndicator())),
+        ],
+      ),
     );
   }
 }
