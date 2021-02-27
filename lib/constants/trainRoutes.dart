b85b9e9b5fa6309b4e30718c725090d5710c6064
SK1n <35175811+SK1n@users.noreply.github.com>
2021-02-27 19:06:22 +0200

    Added first route for trains

---
+++ lib/constants/trainRoutes.dart
@@ -0,0 +1,17 @@
+const String R5620 = ''' 
+Putna 05:04\n
+Putna Centru h. 05:06\n
+Putna Troita h. 05:12\n
+Gura Putnei 05:17\n
+Bivolaria hc. 05:28\n
+Vicovu de Sus hc. 05:28\n
+Vicovu de Jos hc. 05:58\n
+Radauti 06:07\n
+Radauti Gara Mica h. 06:10\n
+Dornesti 06:24\n
+Tibeni hc. 06:36\n
+Milisauti 06:44\n
+Darmanesti 06:55\n
+Suceava Nord 07:06\n
+Suceava 07:12\n
+''';
---
+++ lib/transport/Train.dart
@@ -0,0 +1,63 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapperadauti/constants/trainRoutes.dart';
+
+import '../widgets/src/appBarModel.dart';
+
+class Train extends StatefulWidget {
+  @override
+  _TrainState createState() => _TrainState();
+}
+
+final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+class _TrainState extends State<Train> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBarModel()
+          .loadAppBar(context, 'Trenuri', Icons.train, _scaffoldKey),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              ExpandableNotifier(
+                child: Column(
+                  children: [
+                    ScrollOnExpand(
+                      child: Expandable(
+                        collapsed: ExpandableButton(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width - 20,
+                            child: Text('R5620'),
+                          ),
+                        ),
+                        expanded: ExpandableButton(
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width - 20,
+                                  child: Text('R5620'),
+                                ),
+                                Container(
+                                  child: Text(R5620),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
