4b26269c24debb30bc94f7c160fd058392d17cd2
SK1n <35175811+SK1n@users.noreply.github.com>
2021-03-11 13:03:31 +0200

    added Notice a problem route to the notifications

--- lib/services/router.dart
+++ lib/services/router.dart
@@ -5,6 +5,7 @@ import 'package:flutterapperadauti/constants/route_names.dart';
 import 'package:flutterapperadauti/events/main.dart';
 import 'package:flutterapperadauti/jobs/main_page_jobs.dart';
 import 'package:flutterapperadauti/main_page_model.dart';
+import 'package:flutterapperadauti/notice_a_problem/layout_notice_a_problem.dart';
 import 'package:flutterapperadauti/town_hall/council_meetings.dart';
 import 'package:flutterapperadauti/usefull_numbers/local_authorities.dart';
 
@@ -35,6 +36,11 @@ Route<dynamic> generateRoute(RouteSettings settings) {
         routeName: settings.name,
         viewToShow: AirQualityPage(),
       );
+    case NoticeProblemRoute:
+      return _getPageRoute(
+        routeName: settings.name,
+        viewToShow: LayoutNoticeProblem(),
+      );
     default:
       return _getPageRoute(
         routeName: settings.name,
@@ -42,6 +48,14 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       );
   }
 }
+
+/*
+  ! To add one more route add one more case to the switch above like this:
+    case [RouteName] :  -- this name will be defined in lib/constants/route_names.dart
+    routeName: setting.name, (this line always remains the same)
+    viewToShow: [Screen that we're gonna show] let's say for example MenuScreen(),
+    We're done here now let's go to lib/constants/route_names.dart
+*/
 
 PageRoute _getPageRoute({String routeName, Widget viewToShow}) {
   return MaterialPageRoute(
