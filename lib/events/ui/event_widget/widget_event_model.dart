3abe7a90908b67b1c5cd52e9a3a62a23e6f1e706
luis-fernandoscripcaru <luys50734@gmail.com>
2021-04-08 20:17:54 +0300

    added platform specific version in about app and fixed width in events

--- lib/events/ui/event_widget/widget_event_model.dart
+++ lib/events/ui/event_widget/widget_event_model.dart
@@ -4,7 +4,7 @@ import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
 
-class WidgetEventModel{
+class WidgetEventModel {
   Future<void> _onOpen(LinkableElement link) async {
     if (await canLaunch(link.url)) {
       await launch(link.url);
@@ -12,7 +12,8 @@ class WidgetEventModel{
       throw 'Nu pot încărca $link';
     }
   }
-  Widget widgetScrollOnExpond(child){
+
+  Widget widgetScrollOnExpond(child) {
     return ScrollOnExpand(
       scrollOnExpand: true,
       child: ExpandablePanel(
@@ -32,7 +33,10 @@ class WidgetEventModel{
         ),
         builder: (_, collapsed, expanded) {
           return Padding(
-            padding: EdgeInsets.only(left: 10, right: 10,),
+            padding: EdgeInsets.only(
+              left: 10,
+              right: 10,
+            ),
             child: Expandable(
               expanded: expanded,
               theme: const ExpandableThemeData(),
@@ -42,7 +46,8 @@ class WidgetEventModel{
       ),
     );
   }
-  Widget textColumn1(child, childSize, childFontWeight, childColor){
+
+  Widget textColumn1(child, childSize, childFontWeight, childColor) {
     return Text(
       child,
       textAlign: TextAlign.center,
@@ -53,7 +58,9 @@ class WidgetEventModel{
       ),
     );
   }
-  Widget rowIconColumn3(childText, childIcon, childContext, childPaddingIconLeft, childPaddingTextLeft, childSize1, childSize2){
+
+  Widget rowIconColumn3(childText, childIcon, childContext,
+      childPaddingIconLeft, childPaddingTextLeft, childSize1, childSize2) {
     return Row(
       children: <Widget>[
         Padding(
@@ -62,9 +69,11 @@ class WidgetEventModel{
             child: Icon(childIcon, size: childSize1),
           ),
         ),
-        SizedBox(width: 5.0,),
+        SizedBox(
+          width: 5.0,
+        ),
         Container(
-          width: (MediaQuery.of(childContext).size.width / 5) * 2,
+          width: (MediaQuery.of(childContext).size.width / 5) * 2 - 10,
           child: Padding(
             padding: EdgeInsets.fromLTRB(childPaddingTextLeft, 5.0, 0.0, 0.0),
             child: Text(
@@ -82,30 +91,36 @@ class WidgetEventModel{
       ],
     );
   }
-  Widget column1(child, childContext, childSize1, childSize2, childSize3){
+
+  Widget column1(child, childContext, childSize1, childSize2, childSize3) {
     return Column(
       children: <Widget>[
-        textColumn1(child.day + '\n' + child.month, childSize1, FontWeight.bold, Colors.grey[800]),
-        textColumn1('----------', childSize2, FontWeight.normal, Colors.grey[800]),
+        textColumn1(child.day + '\n' + child.month, childSize1, FontWeight.bold,
+            Colors.grey[800]),
+        textColumn1(
+            '----------', childSize2, FontWeight.normal, Colors.grey[800]),
         new DecoratedBox(
           decoration: new BoxDecoration(
             color: Colors.grey[700],
             borderRadius: BorderRadius.circular(2.0),
           ),
           child: new Padding(
-            padding: const EdgeInsets.only(left: 5, right: 5),
-            child: textColumn1(child.hour, childSize3, FontWeight.normal, Colors.white)
-          ),
+              padding: const EdgeInsets.only(left: 5, right: 5),
+              child: textColumn1(
+                  child.hour, childSize3, FontWeight.normal, Colors.white)),
         ),
       ],
     );
   }
-  Widget column2(child, childContext){
+
+  Widget column2(child, childContext) {
     return Column(
       children: <Widget>[
         Padding(
-          padding: const EdgeInsets.only(left: 10,),
-          child:Container(
+          padding: const EdgeInsets.only(
+            left: 10,
+          ),
+          child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               border: Border.all(
@@ -124,11 +139,14 @@ class WidgetEventModel{
                 width: MediaQuery.of(childContext).size.width / 5,
               ),
             ),
-          ),),
+          ),
+        ),
       ],
     );
   }
-  Widget column3(child, childContext, childRow4, childSize1, childSize2, childSize3){
+
+  Widget column3(
+      child, childContext, childRow4, childSize1, childSize2, childSize3) {
     return Column(
       children: <Widget>[
         Row(
@@ -152,9 +170,18 @@ class WidgetEventModel{
             ),
           ],
         ),
-        rowIconColumn3('Categoria: ' + child.categoryName, Icons.category, childContext, 10.0, 0.0, childSize2, childSize3),
-        rowIconColumn3('Organizator: ' + child.organization, Icons.account_circle, childContext, 10.0, 0.0, childSize2, childSize3),
-        rowIconColumn3(child.location, Icons.location_on, childContext, 5.0, 5.0, childSize2, childSize3),
+        rowIconColumn3('Categoria: ' + child.categoryName, Icons.category,
+            childContext, 10.0, 0.0, childSize2, childSize3),
+        rowIconColumn3(
+            'Organizator: ' + child.organization,
+            Icons.account_circle,
+            childContext,
+            10.0,
+            0.0,
+            childSize2,
+            childSize3),
+        rowIconColumn3(child.location, Icons.location_on, childContext, 5.0,
+            5.0, childSize2, childSize3),
         childRow4,
       ],
     );
