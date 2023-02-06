a97f44efa399074a88d03a9666633c8756b4220d
SK1n <luys50734@gmail.com>
2023-02-06 20:21:05 +0200

    added switch to beta

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# fail if any command fails
+set -e
+# debug log
+set -x
+
 # by default, the execution directory of this script is the ci_scripts 
 directory
 # CI_WORKSPACE is the directory of your cloned repo
@@ -7,12 +12,16 @@ echo "🟩 Navigate from ($PWD) to ($CI_WORKSPACE)"
 cd $CI_WORKSPACE
 
 echo "🟩 Install Flutter"
-time git clone https://github.com/flutter/flutter.git -b beta 
+time git clone https://github.com/flutter/flutter.git -b stable 
 $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 echo "🟩 Flutter Precache"
 time flutter precache --ios
+
+echo "🟩 Switching to channel beta"
+time flutter channel beta
+time flutter upgrade
 
 echo "🟩 Install Flutter Dependencies"
 time flutter pub get
@@ -25,6 +34,7 @@ time HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 
 echo "🟩 Install CocoaPods dependencies..."
 time cd ios
-time pod deintegrate && pod install
+time pod deintegrate
+time pod install 
 
 exit 0
