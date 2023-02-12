88e98248077703c660544b32dfc3d6a1585def07
SK1n <35175811+SK1n@users.noreply.github.com>
2023-02-12 21:13:25 +0200

    Ci (#14)
    * ci_init
    
    * fixed cmd line
    
    * added pod deintegrate
    
    * added switch to beta
    
    * removed directory
    
    * chnaged dir
    
    * upd version
    
    * deleted post_install

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+# fail if any command fails
+set -e
+# debug log
+set -x
+
 # by default, the execution directory of this script is the ci_scripts 
-directory
 # CI_WORKSPACE is the directory of your cloned repo
 echo "🟩 Navigate from ($PWD) to ($CI_WORKSPACE)"
 cd $CI_WORKSPACE
 
 echo "🟩 Install Flutter"
-time git clone https://github.com/flutter/flutter.git -b stable 
-$HOME/flutter
+git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
 echo "🟩 Flutter Precache"
 time flutter precache --ios
+
+echo "🟩 Switching to channel beta"
+time flutter channel beta
+time flutter upgrade
 
 echo "🟩 Install Flutter Dependencies"
 time flutter pub get
@@ -24,6 +31,8 @@ echo "🟩 Install CocoaPods via Homebrew"
 time HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
 
 echo "🟩 Install CocoaPods dependencies..."
-time cd ios && && rm Podfile.lock && pod install
+time cd ios
+time pod deintegrate
+time pod install 
 
 exit 0
