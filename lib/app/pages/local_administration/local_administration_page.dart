28ff0ef273525aa734ae633cc9d7c352ee907182
SK1n <luys50734@gmail.com>
2023-06-22 15:28:35 +0300

    added auto router

--- lib/app/pages/local_administration/local_administration_page.dart
+++ lib/app/pages/local_administration/local_administration_page.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class LocalAdministrationPage extends StatelessWidget {
   const LocalAdministrationPage({super.key});
 
+  static Route<void> route() {
+    return MaterialPageRoute<void>(
+        builder: (_) => const LocalAdministrationPage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
