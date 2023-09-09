bc8b629ac0c9952fb48b5302e1f0bf59da6e5d54
SK1n <luys50734@gmail.com>
2023-09-09 23:43:29 +0300

    fixed search issue

--- lib/app/pages/local_administration/cubit/decisions/local_decisions_cubit.dart
+++ lib/app/pages/local_administration/cubit/decisions/local_decisions_cubit.dart
@@ -1,4 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
+import 'package:fuzzywuzzy/model/extracted_result.dart';
+import 'package:logger/logger.dart';
 import '../../../../models/local_administration/decision_model.dart';
 import '../../../../repository/local_administration/local_administration_repository.dart';
 import '../../../../utils/page_state.dart';
@@ -41,9 +44,18 @@ class LocalDecisionsCubit extends Cubit<LocalDecisionsState> {
 
   void filterLocalDecisions(String query) {
     emit(state.copyWith(state: PageState.inProgress));
-    var data = state.data
-        .where((element) => element.title.toLowerCase().contains(query))
-        .toList();
+    List<DecisionModel> data = [];
+    var processedData = extractAllSorted(
+      query: query,
+      choices: state.data.map((e) => e.title).toList(),
+      cutoff: 50,
+    );
+    for (var element in processedData) {
+      data.add(state.data[element.index]);
+    }
+    Logger log = Logger();
+    log.d(data);
+
     emit(
       state.copyWith(
         state: PageState.success,
